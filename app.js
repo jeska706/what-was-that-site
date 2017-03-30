5e84d9bdf5a7647c4216f7b065366d12b8be0d05
jeska706 <jessicabeardsley@hotmail.com>
2017-03-30 16:40:58 -0600

    Created new link function

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ app.controller('MainCtrl', ['$scope', function($scope){
     $scope.test = 'controller is connected';
 
     $scope.links = [
-        {title: "link 1", stars: 5},
-        {title: "link 2", stars: 10},
-        {title: "link 3", stars: 2},
-        {title: "link 4", stars: 2},
-        {title: "link 5", stars: 8},
+        {address: "link 1", stars: 5},
+        {address: "link 2", stars: 10},
+        {address: "link 3", stars: 2},
+        {address: "link 4", stars: 2},
+        {address: "link 5", stars: 8},
     ];
 
-    
+    $scope.addLink = function(){
+        $scope.links.push({address: "A new link", stars: 0});
+    };
 }]);
